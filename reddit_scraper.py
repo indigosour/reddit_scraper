c0378fa59454bd6c023aa8ba6109aa720792d275
indigosour <48144817+indigosour@users.noreply.github.com>
2022-07-04 16:34:44 -0500

    Add get URL back to filter non videos

--- reddit_scraper.py
+++ reddit_scraper.py
@@ -1,11 +1,17 @@
 import praw
 import uuid
 import os
+import glob
 from redvid import Downloader
+import ffmpeg
+import requests
 
 reddit_read_only = praw.Reddit(client_id="uM6URp2opqPfANoCdPE09g",         # your client id
                                client_secret="ofL3-C58gmXaHgiGHYJ_Mx4MdmOd3w",      # your client secret
                                user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36")        # your user agent
+
+# Variables
+working_dir = (os.path.dirname(os.path.realpath(__file__))) + "/storage/"
 
 # Get reddit list for <sub> - videos,tiktokcringe,etc and <period> - day, week, month, year
 # Output: json object containing id, title, author, score, permalink, and flare
@@ -25,8 +31,34 @@ def get_reddit_list(sub,period):
         })
     return postlist
 
-# Download MP4 and combine video/audio with ffmpeg
+# Check for videos and collect list
 
+def get_mp4_urllist(sub,period):
+    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+    mp4_urllist = []
+    #sub = 'funny'
+    #period = 'day'
+    for reddit_url in get_reddit_list(f'{sub}',f'{period}'):
+        if reddit_url["permalink"]:
+            if reddit_url["permalink"][len(reddit_url["permalink"])-1] == '/':
+                json_url = reddit_url["permalink"][:len(reddit_url["permalink"])-1]+'.json'
+            else:
+                json_url = reddit_url["permalink"] + '.json'
+            json_response = requests.get(json_url, 
+                            headers= headers)
+            if json_response.json()[0]['data']['children'][0]['data']['is_video'] == True:
+                json_urllist = {
+                    "id": json_response.json()[0]['data']['children'][0]['data']['id'],
+                    "permalink": reddit_url["permalink"],
+                    }
+                mp4_urllist.append(json_urllist)
+            elif json_response.status_code != 200:
+                print("Error Detected, check the URL!!!")
+
+    return mp4_urllist
+
+
+# Download MP4 - v.reddit.it
 def download_video(url,path):
     reddit = Downloader(max_q=True)
     reddit.url = url
@@ -35,11 +67,21 @@ def download_video(url,path):
 
 # Input subreddit and period of time to create working directory and collect mp4 files
 def main(sub,period):
+    #sub = 'funny'
+    #period = 'day'
+    global working_dir
     playlist = get_reddit_list(f'{sub}',f'{period}')
     uuid_value = str(uuid.uuid4())
-    parent_dir = "/home/john/code_repo/reddit_scraper/storage/"
+    parent_dir = working_dir
     path = os.path.join(parent_dir, uuid_value)
     os.mkdir(path)
 
     for post in playlist:
+        print (post["permalink"])
         download_video(post["permalink"],path)
+
+    #filenames = glob.glob(f'/home/john/code_repo/reddit_scraper/storage/{uuid_value}/*.mp4')
+    #return filenames
+
+# subprocess.run(["cat", "*.mp4","|","ffmpeg","-i","pipe:","-c:a","copy","-c:v","copy all.mp4"])
+# cat *.mp4  | ffmpeg  -i pipe: -c:a copy -c:v copy all.mp4
