fa658528e247923dd5b33d7401ad635bdc996ec4
indigosour <48144817+indigosour@users.noreply.github.com>
2023-03-27 05:46:45 +0000

    Update host

--- worker.py
+++ worker.py
@@ -1,16 +1,26 @@
 import pika
+from main import grab_dat
+from database import process_subreddit_update
 
 # Define callback function to process received messages
 def callback(ch, method, properties, body):
-    print(f"Received message: {body}")
+    message = body.decode()
+    print(f"Received message: {message}")
+
+    if message == "":
+        grab_dat()
+    elif message == "process_subreddit_update":
+        process_subreddit_update()
+    else:
+        print(f"Unknown message: {message}")
 
 # Connection and channel creation
-connection_params = pika.ConnectionParameters(host='localhost')
+connection_params = pika.ConnectionParameters(host='172.19.0.2')
 connection = pika.BlockingConnection(connection_params)
 channel = connection.channel()
 
 # Declare the queue (if it doesn't exist, it will be created)
-queue_name = 'toDownload'
+queue_name = 'work'
 channel.queue_declare(queue=queue_name)
 
 # Bind the callback function to the queue and start consuming messages
