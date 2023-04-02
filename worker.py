e59e4b1db40b3209a07511adf9db409637f657a8
indigosour <48144817+indigosour@users.noreply.github.com>
2023-04-02 19:44:51 +0000

    Converted to async operation so RMQ connection stays alive and for best practice

--- worker.py
+++ worker.py
@@ -1,4 +1,8 @@
-import json, pika
+import asyncio
+import json
+import logging
+from aio_pika import connect_robust, Message, DeliveryMode, ExchangeType
+
 from common import *
 from main import *
 
@@ -6,59 +10,72 @@ from main import *
 heartbeat_interval = 60
 socket_timeout = 2 * heartbeat_interval
 
-def connect_rabbit(queue='work'):
-    mq_cred = pika.PlainCredentials(get_az_secret('RMQ-CRED')['username'], get_az_secret('RMQ-CRED')['password'])
-    connection = pika.BlockingConnection(pika.ConnectionParameters(
-        host=get_az_secret('RMQ-CRED')['url'],
-        credentials=mq_cred,
-        heartbeat=heartbeat_interval,
-        socket_timeout=socket_timeout
-    ))
-    channel = connection.channel()
-    channel.queue_declare(queue=queue, durable=True)
+
+async def connect_rabbit(queue='work'):
+    rmq_url = get_az_secret('RMQ-CRED')['url']
+    username = get_az_secret('RMQ-CRED')['username']
+    password = get_az_secret('RMQ-CRED')['password']
+
+    connection_string = f"amqp://{username}:{password}@{rmq_url}?heartbeat={heartbeat_interval}"
+    connection = await connect_robust(connection_string)
+    
+    channel = await connection.channel()
+    await channel.set_qos(prefetch_count=1)
+    await channel.declare_queue(queue, durable=True)
     return connection, channel
 
 
-def process_dl_period(period, p_id, batch):
+async def process_dl_period(period, p_id, batch):
     try:
-        main_dl_period(period, p_id, batch)
+        await asyncio.to_thread(main_dl_period, period, p_id, batch)
         clear_tmp_folder()
         print(f"Completed processing batch")
     except Exception as e:
         print(f"Error processing batch {e}")
 
 
-def on_message(channel, method, properties, body):
-    headers = properties.headers
-    if headers['job_type'] == 'dl_period':    
-        p_id = headers['p_id']
-        period = headers['period']
-        json_batch = json.loads(body)
-        print(f"Received: a message")
-        process_dl_period(period, p_id, json_batch)
-        channel.basic_ack(delivery_tag=method.delivery_tag)
-    elif headers['job_type'] != 'dl_period':
-        print("Unknown job type, skipping message processing")
-        logging.info("Unknown job type, skipping message processing")
-        channel.basic_nack(delivery_tag=method.delivery_tag, requeue=True)
+async def on_message(message):
+    async with message.process():
+        headers = message.headers
+        if headers['job_type'] == 'dl_period':
+            p_id = headers['p_id']
+            period = headers['period']
+            json_batch = json.loads(message.body)
+            print(f"Received: a message")
+            await process_dl_period(period, p_id, json_batch)
+
+            # Acknowledge the message after processing
+            await message.ack()
+        elif headers['job_type'] != 'dl_period':
+            print("Unknown job type, skipping message processing")
+            logging.info("Unknown job type, skipping message processing")
+            await message.reject(requeue=True)
 
 
-def main():
-    # Connect to RabbitMQ and start consuming messages
-    connection, channel = connect_rabbit()
-    channel.basic_qos(prefetch_count=5)
-    channel.basic_consume(queue='work', on_message_callback=on_message)
+async def main():
+    event = asyncio.Event()
+    while True:
+        try:
+            # Connect to RabbitMQ and start consuming messages
+            connection, channel = await connect_rabbit()
+            queue = await channel.declare_queue('work', durable=True)
+            await queue.consume(on_message)
 
-    print("Waiting for messages. Press CTRL+C to exit.")
+            print("Waiting for messages. Press CTRL+C to exit.")
+            await event.wait()  # Use the created Event instance here
+        except (KeyboardInterrupt, Exception) as e:
+            if isinstance(e, KeyboardInterrupt):
+                print("Exiting gracefully...")
+                break
+            print(f"Error: {e}")
+            print("Attempting to reconnect in 10 seconds...")
+            await asyncio.sleep(10)
+        finally:
+            try:
+                await connection.close()
+            except Exception as e:
+                print(f"Error closing connection: {e}")
 
-    try:
-        channel.start_consuming()
-    except (KeyboardInterrupt, Exception) as e:
-        print(e)
-        print("Exiting gracefully...")
-    finally:
-        channel.stop_consuming()
-        connection.close()
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
